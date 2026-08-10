88adeed430804ea7982911c2b38fe29cb9d2293d
vivek <vivekdamar28@gmail.com>
2026-08-10 15:10:11 +0530

    Update project

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { config } from './config';
 
 const app = buildServer();
 
-app.listen({ port: config.port }, (err) => {
+app.listen({ port: config.port, host: '0.0.0.0' }, (err) => {
   if (err) {
     app.log.error(err);
     process.exit(1);
